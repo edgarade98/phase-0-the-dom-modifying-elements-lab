8b0f89445c3326de837365ae66e9fad735357983
Edgar <eddyade98@gmail.com>
2023-10-24 16:29:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+// Set the id of the <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Add some text content to the <h1> element
+newHeader.textContent = "YOUR-NAME is the champion"; 
+
